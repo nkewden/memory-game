ac7c91ac9eede8cf186125dde5cac01e421d9fa3
nkewden <nkewden@gmail.com>
2022-03-20 21:28:40 -0500

    Updated script.js, index.html and README.md

--- memory-game/script.js
+++ memory-game/script.js
@@ -1,15 +1,20 @@
 // global constants
-const clueHoldTime = 80; //how long each clue plays for 
 const cluePauseTime = 250; //the time difference between clues
+const len = 4;
 const nextClueWaitTime = 1000; //time until playback of clue sequence
 
 //Global Variables
 var pattern = [2, 6, 4, 3, 2, 5, 2, 4, 5, 3]; //stores secret pattern of button presses
 var progress = 0; //stores how far along the player is guessing the pattern
+var clueHoldTime = 300; //how long each clue plays for 
 var gamePlaying = false; //boolean: if game is active
 var tonePlaying = false;
 var volume = 0.7;  
 var guessCounter = 0;
+var mistakes = 0;
+var count = 10;
+var reset = false;
+let timer = null;
 
 //this function initialize game variables
 function startGame(){
@@ -25,6 +30,12 @@ function stopGame(){
   gamePlaying = false;
   document.getElementById("stopBtn").classList.add("hidden");
   document.getElementById("startBtn").classList.remove("hidden");
+}
+
+function generatePattern() {
+  for (let j = 0; j < len; j++) {
+    pattern[j] = Math.ceil(Math.random() * 4);
+  }
 }
 
 // Sound Synthesis Functions
@@ -95,7 +106,34 @@ function playClueSequence(){
     delay -= clueHoldTime 
     delay += cluePauseTime;
   }
+  clueHoldTime -= 100;
+  count = 10;
+  reset = false;
+
+  clearInterval(timer);
+  timer = setInterval(Timer, 1000);
 }
+
+//decreases the count every one second
+function Timer() {
+    document.getElementById("clock").innerHTML =
+      "Countdown: " + count + " s";
+    count -= 1; 
+    if (count < 0 || reset) {
+      if(!reset) {
+        stopGame(); 
+        alert("Time is up. You lost.");
+      }
+      resetTimer();
+      clearInterval(timer);
+    }
+}
+function resetTimer(){
+  count = 10;
+  document.getElementById("clock").innerHTML = "Countdown: 0 s";
+}
+
+
 
 function loseGame(){
   stopGame();
@@ -108,25 +146,35 @@ function winGame(){
 }
 
 //logic function to check each guess 
-function guess(btn){
+function guess(btn) {
   console.log("user guessed: " + btn);
-  if(!gamePlaying){
+  if (!gamePlaying) {
     return;
   }
-  
-  //Guess was correct
-  if(pattern[guessCounter] == btn){
-    if(guessCounter == progress){
-      if(progress == pattern.length - 1){
-        winGame(); //You win game!
-      }else{
+
+  // add game logic here
+  if (btn === pattern[guessCounter]) {
+    if (guessCounter === progress) {
+      if (progress === pattern.length - 1) {
+        winGame(); //You win game
+        reset = true;
+      } else {
+        //Pattern correct next segment
         progress++;
-        playClueSequence(); //add next segment
+        playClueSequence();
       }
-    }else{
-      guessCounter++; //checking the next guess
+    } else {
+      guessCounter++; //checking next guess
     }
-  }else{
-    loseGame(); //you lose if guess is incorrect
+  } else {
+    //guess wrong
+    mistakes++;
+    if (mistakes === 3) {
+      loseGame(); // You Lose game
+      reset = true;
+    } else {
+      alert("Wrong! Attempts left:" + (3 - mistakes));
+    }
   }
 }
+
